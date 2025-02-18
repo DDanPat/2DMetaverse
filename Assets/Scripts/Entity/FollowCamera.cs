9006aa239d1763d6c36c9c46d3ef6faa4a9669fc
DDanPat <hyoung6855@gmail.com>
2025-02-18 15:34:33 +0900

    [Update] FollowCamera 조건문 추가
    FollowCamera에 조건문을 추가해서 MainScene에서는 x축과 y축 모두 반영하고 FlappyBird 씬에서는 x축만 반영하여 카메라가 플레이어를 따라오게 만듬

--- Assets/Scripts/Entity/FollowCamera.cs
+++ Assets/Scripts/Entity/FollowCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FollowCamera : MonoBehaviour
 {
@@ -23,8 +24,17 @@ public class FollowCamera : MonoBehaviour
             return;
 
         Vector3 pos = transform.position;
-        pos.x = target.position.x + offsetX;
-        pos.y = target.position.y + offsetY;
+
+        if (SceneManager.GetActiveScene().name == "MainScene")
+        {
+            pos.x = target.position.x + offsetX;
+            pos.y = target.position.y + offsetY;
+        }
+        else if (SceneManager.GetActiveScene().name == "FlappyBird")
+        {
+            pos.x = target.position.x + offsetX;
+        }
+        
         transform.position = pos;
     }
 
