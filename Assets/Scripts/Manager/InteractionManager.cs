6661a22d386521f029142d63747645b45a07c661
DDanPat <hyoung6855@gmail.com>
2025-02-18 10:41:56 +0900

    [Update] InteractionManager.cs 제작
    동상과 포탈에 닿았을때 이벤트 트리거 작동 스크립트 제작

--- Assets/Scripts/Manager/InteractionManager.cs
+++ Assets/Scripts/Manager/InteractionManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Debug.Log("Statue On");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log("Potal On");
     }
 }
